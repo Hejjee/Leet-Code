c4800388ca8cb24e618bb300af0fbda268e3f7bc
Hejjee <imraanbarotov@gmail.com>
2022-09-03 23:36:14 +0300

    __init__

---
+++ reverseStringLeetCode.py
@@ -0,0 +1,8 @@
+def reverseString(s):
+    """
+        Do not return anything, modify s in-place instead.
+        """
+    s = s[::-1]
+    return s
+    
+print(reverseString(["H", "e", "l", "l", "o", "D", "u", "r", "a"]))
